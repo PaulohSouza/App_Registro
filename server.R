e2fb2aa17eaa5348146b9652f821fe57eb97d7f5
Paulo Souza <souza.phn.agro@gmail.com>
2022-09-15 11:53:32 -0400

    Atualizacao 1.0

---
+++ server.R
@@ -0,0 +1,38 @@
+﻿library(gsheet)
+library(openxlsx)
+
+df <- as.data.frame(gsheet2tbl("https://docs.google.com/spreadsheets/d/1Rw92CBAn4Th4wYp1R1ecYgWH1ypUDl0y/edit#gid=2079184000"), header = TRUE)
+
+library(shiny)
+
+
+Salvar <- function(df){
+  
+  output_path <- "https://docs.google.com/spreadsheets/d/1Rw92CBAn4Th4wYp1R1ecYgWH1ypUDl0y/edit#gid=2079184000"
+  wb <- createWorkbook(output_path) 
+  saveWorkbook(wb, output_path, overwrite=TRUE)
+  #writeDataTable("https://docs.google.com/spreadsheets/d/1Rw92CBAn4Th4wYp1R1ecYgWH1ypUDl0y/edit#gid=2079184000", df)
+  #openxlsx::write.xlsx(df, file = "https://docs.google.com/spreadsheets/d/1Rw92CBAn4Th4wYp1R1ecYgWH1ypUDl0y/edit#gid=2079184000", rowNames = FALSE)
+  
+}
+Salvar(df)
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+    
+    observeEvent(input$IN_ENVIAR, {
+      # INDICE <- nrow(df)
+      # INS <- c(INDICE, as.character(input$IN_DATA_OCORRENCIA), input$IN_USUARIO, input$IN_DEPARTAMENTO,
+      #        input$IN_PROBLEMA, input$IN_CAUSA, input$IN_SOLUCAO, input$IN_RESPONSAVEL)
+         showNotification("Prezados - Aqui é um aplicativo teste - Após o clique será inserido na planilha do Drive! - Por segurança na planilha, Não habilitei a função ainda! (Paulo)")
+      # df[nrow(df) + 1,] <- INS
+      #INS <- append(INS, after = length(INS))
+      #Salvar(df)
+    })
+  
+    output$OUT_REGISTROS <- DT::renderDataTable({
+            df
+
+    })
+    
+    
+})
---
+++ ui.R
@@ -0,0 +1,61 @@
+﻿library(shiny)
+library(DT)
+library(gsheet)
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Controle de ocorrências"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      dateInput("IN_DATA_OCORRENCIA", 
+                h3("Selecione a data de ocorrência"), 
+                value = "2022-01-01"),  
+      selectInput("IN_USUARIO", h3("Escolha o usuário: "), 
+                  choices = list("Felipe Araújo" = "Felipe Araújo",
+                                 "Paulo Souza" = "Paulo Souza",
+                                 "Larissa Santos" = "Larissa Santos"),
+                  selected = "Felipe Araújo"),
+      selectInput("IN_DEPARTAMENTO", h3("Informe o Departamento: "), 
+                  choices = list("Operacional" = "Operacional",
+                                 "Pesquisa" = "Pesquisa"),
+                  selected = "Operacional"),
+      radioButtons("IN_CAUSA", h3("Selecione a causa:"),
+                   choices = list("Logística" = "Logística",
+                                  "Atraso" = "Atraso",
+                                  "Falta de insumos" = "Insumos")
+      ),
+      selectInput("IN_RESPONSAVEL", h3("Escolha o responsável direto: "), 
+                  choices = list("Felipe Araújo" = "Felipe Araújo",
+                                 "Paulo Souza" = "Paulo Souza",
+                                 "Larissa Santos" = "Larissa Santos")),
+      dateInput("IN_DATA_INFORME", 
+                h3("Selecione a data atual:"), 
+                value = "2022-01-01"),  
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Novo Registro",
+                 textAreaInput("IN_PROBLEMA", "Descreva o Problema: ", "informe o problema..", width = "1000px", height = 200),
+                 br(),
+                 textAreaInput("IN_SOLUCAO", "Sugira algumas soluções: ", "Informe suas propostas de soluções'", width = "1000px", height = 200),
+                 tags$head(tags$script(src = "message-handler.js")),
+                 actionButton("IN_ENVIAR", "Enviar ", class="btn btn-success", icon= icon("angle-right"))
+        ),
+        tabPanel("Registros",
+                 
+                 DT::dataTableOutput("OUT_REGISTROS")
+        )
+      )
+    )
+  )
+ 
+ )
+)
